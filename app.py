431a026465bf83698b31809b97a23b818d70d340
unknown <dk6304273089@gmail.com>
2021-09-21 13:24:01 +0530

    deleted setup.sh

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ if ok:
     df=read_data(r"{}".format(name))
     z=df
     df=drop_data(df)
-    model=joblib.load(r"C:\Users\Dheeraj kumar\OneDrive\Desktop\aps\saved_models\model.joblib")
+    model=joblib.load("saved_models/model.joblib")
     z["class"]=model.predict(df)
     z.to_csv( name1 + '\\output_'  + '.csv' , index=False )
-    st.subheader("the Process Complete. Please check Output Directory")
+    st.subheader("PROCESS COMPLETED. PLEASE CHECK OUTPUT DIRECTORY")
 
