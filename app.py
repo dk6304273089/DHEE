01fb90833506f6d67dc65284a4995bb05a89f798
unknown <dk6304273089@gmail.com>
2022-02-27 20:35:49 +0530

    updated app.py and readme.md

--- app.py
+++ app.py
@@ -14,9 +14,6 @@ try:
     input5=col5.sidebar.number_input("Enter the value of dn_000",min_value=0,max_value=2924584)
     input6=col6.sidebar.number_input("Enter the value of cj_000",min_value=0,max_value=60949671)
     ok = st.button("SUBMIT")
-
-
-
 #aa_000 = int(st.sidebar.number_input("Enter the value of aa_000",min_value=0,max_value=2746564))
 #ag_002=int(st.sidebar.number_input("Enter the value of ag_002",min_value=0,max_value=10552856))
 #cs_004=int(st.sidebar.number_input("Enter the value of cs_004",min_value=0,max_value=74860628))
